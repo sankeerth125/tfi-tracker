0b51d9033a278802697aa4be2acf03ffeb6f2bf9
sankeerth125 <sankeerth25199@gmail.com>
2026-01-04 17:26:49 +0530

    fixed port issue in index.ts

--- server/index.ts
+++ server/index.ts
@@ -85,15 +85,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    }
-  );
+  const port = Number(process.env.PORT) || 5000;
+  httpServer.listen(port, () => {
+    log(`serving on port ${port}`);
+  });
 })();
